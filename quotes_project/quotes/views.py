c1eff2a3c5d678d4484a6ce8b8d48acbc8ca74a7
iamleeon <165953114+iamleeon@users.noreply.github.com>
2024-09-01 15:16:18 +0300

    module 10

--- quotes_project/quotes/views.py
+++ quotes_project/quotes/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
+
+from .utils import get_mongodb
 
 
 # Create your views here.
-def main(request):
-    return render(request, "quotes/index.html", context={})
+def main(request, page=1):
+    db = get_mongodb()
+    quotes = db.quotes.find()
+    quotes_per_page = 10
+    paginator = Paginator(list(quotes), quotes_per_page)
+    quotes_displayed_on_page = paginator.page(page)
+    return render(request, "quotes/index.html", context={"quotes": quotes_displayed_on_page })
